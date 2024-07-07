247bf0adb021da7f93562ef518a173130ca25f3e
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2024-07-07 10:14:07 -0500

    Update launcher icons and README
    Added new adaptive icons for all densities and updated Android Studio version in README.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Android Studio Version
+
+Android Studio Giraffe | 2022.3.1 Patch 2
--- src/utils/notificationService.js
+++ src/utils/notificationService.js
@@ -12,7 +12,7 @@ const body = "Es tiempo de guardar tus gastos";
 if (isAndroid) {
   PushNotification.createChannel({
     channelId: constants.channelId,
-    channelName: "Money Manager",
+    channelName: "Mister Toby",
     playSound: true,
     soundName: "default",
   });
